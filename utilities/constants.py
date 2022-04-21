34e5a7b2fdd34b6f8dfedde5ff1e1f8a9fa4e134
mdhanda <manuraj.dhanda@gmail.com>
2022-04-21 18:30:44 +1000

    imports updated in requirements.txt

--- utilities/constants.py
+++ utilities/constants.py
@@ -5,4 +5,4 @@ CSV_FILENAME = 'course_data.csv'
 MONGO_CONNECTION_URL = "mongodb+srv://mongodb:mongodb@cluster0.2wjy8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 DB_NAME = "iNeuron_Assignments"
 COLLECTION_NAME = "Neuron_Courses"
-DRIVER_PATH = 'chromedriver.exe'
+DRIVER_PATH = 'chromedriver'
